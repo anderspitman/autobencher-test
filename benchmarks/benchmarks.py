991d5465c85575192f8427a866e03c6ff162601a
Anders Pitman <tapitman11@gmail.com>
2016-05-11 14:04:35 -0700

    Use dummy for bench

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,8 +1,8 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+from dummy import *
 
 class TimeSuite:
     def time_a(self):
-        for i in range(100):
-            pass
+        func()
