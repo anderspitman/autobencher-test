1b60a931fd6c0a74cfd3a03f9efb670dafd4c16c
Anders Pitman <tapitman11@gmail.com>
2016-05-11 13:52:52 -0700

    Initial commit

---
+++ benchmarks/benchmarks.py
@@ -0,0 +1,8 @@
+# Write the benchmarking functions here.
+# See "Writing benchmarks" in the asv docs for more information.
+
+
+class TimeSuite:
+    def time_a(self):
+        for i in range(100):
+            pass
