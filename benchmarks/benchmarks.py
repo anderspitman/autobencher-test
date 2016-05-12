3cf1b88d94d47a759044e200a0ccdc6916ab7b39
Anders Pitman <tapitman11@gmail.com>
2016-05-12 13:38:04 -0700

    maybe

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,14 +1,14 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
-from dummy import func1, func2, func3
+from abtest import func1, func2, func3
 
 class TimeSuite:
     def time_func1(self):
-        func1()
+        dummy.func1()
 
     def time_func2(self):
-        func2()
+        dummy.func2()
 
     def time_func3(self):
-        func3()
+        dummy.func3()
