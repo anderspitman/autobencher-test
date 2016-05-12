040c3aa1f56d8ede4eb6074a1351fe228a0939f4
Anders Pitman <tapitman11@gmail.com>
2016-05-12 14:34:51 -0700

    slwoer

--- abtest.py
+++ abtest.py
@@ -1,11 +1,11 @@
 def func1():
-    for i in range(10000):
+    for i in range(100000):
         pass
     
 def func2():
-    for i in range(10000):
+    for i in range(100000):
         pass
 
 def func3():
-    for i in range(100000):
+    for i in range(1000000):
         pass
